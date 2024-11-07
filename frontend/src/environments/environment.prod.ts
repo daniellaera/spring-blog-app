a7ae771d8815d8de099cbfdc6b7eef7f683e31a1
Daniel Laera <laera.daniel@hotmail.com>
2024-11-07 17:30:43 +0100

    final thoughts

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,9 @@
+const parseApiUrl = (url: string) => {
+  // In this case, we expect URL to be a string, but you could modify for other types if needed
+  return url;
+};
+
 export const environment = {
   production: true,
-  apiUrl: '${API_URL}'
+  apiUrl: parseApiUrl('${API_URL}')
 };
