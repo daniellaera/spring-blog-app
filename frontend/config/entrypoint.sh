c60f47c76b42b9402f1fb0f582dcf8dbad96b51e
Daniel Laera <laera.daniel@hotmail.com>
2024-11-07 17:06:50 +0100

    try those shit

--- frontend/config/entrypoint.sh
+++ frontend/config/entrypoint.sh
@@ -1,18 +1,12 @@
 #!/bin/sh
 
-# Ensure that API_URL is set in the environment variables
+# Ensure API_URL is available and replace it in the HTML/JS files before starting Nginx
 if [ -n "$API_URL" ]; then
-    echo "Replacing API_URL in JavaScript files with value: $API_URL"
+    echo "Replacing API_URL in the app with: $API_URL"
 
-    # Replace ${API_URL} in all JS files in /usr/share/nginx/html with the actual value of API_URL
-    find /usr/share/nginx/html -type f -name "*.js" -exec sed -i "s|\${API_URL}|$API_URL|g" {} \;
-
-    # Optionally, print the first few lines of the first JS file to confirm
-    echo "First few lines of the first JS file after replacement:"
-    head -n 10 $(find /usr/share/nginx/html -type f -name "*.js" | head -n 1)
-else
-    echo "API_URL environment variable is not set!"
+    # Replace the placeholder API_URL in JavaScript files
+    find /usr/share/nginx/html -type f -name "*.js" -exec sed -i "s|\${API_URL}|$API_URL|" {} \;
 fi
 
-# Run the default command (Nginx)
+# Start Nginx in the foreground
 exec "$@"
