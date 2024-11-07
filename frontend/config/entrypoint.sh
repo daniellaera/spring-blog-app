650b8f309df34b3be5e647e3064bd774a7cc8867
Daniel Laera <laera.daniel@hotmail.com>
2024-11-07 16:49:36 +0100

    new config

--- frontend/config/entrypoint.sh
+++ frontend/config/entrypoint.sh
@@ -1,28 +1,17 @@
 #!/bin/sh
 
-# Replacement for the dev environment (targeting environment.development.ts)
-if [ -n "$API_URL" ] && [ -f 'src/environments/environment.development.ts' ]; then
-    echo "Replacing API_URL in environment.development.ts..."
-
-    # Replacing the API URL in the development environment configuration
-    sed -i "s|apiUrl: '.*',|apiUrl: '${API_URL}',|" src/environments/environment.development.ts
-fi
-
-# Replacement for the prod environment (targeting environment.ts)
+# replacement for the dev environment / target
 if [ -n "$API_URL" ] && [ -f 'src/environments/environment.ts' ]; then
-    echo "Replacing API_URL in environment.ts..."
+    echo "Replacing API_URL for dev..."
 
-    # Replacing the API URL in the production environment configuration
     sed -i "s|apiUrl: '.*',|apiUrl: '${API_URL}',|" src/environments/environment.ts
 fi
 
-# Replacement for the prod environment (targeting /usr/share/nginx/html/*.js files)
+# replacement for the prod environment / target
 if [ -n "$API_URL" ] && [ -d '/usr/share/nginx/html' ]; then
-    echo "Replacing API_URL in JavaScript files in /usr/share/nginx/html..."
+    echo "Replacing API_URL for prod..."
 
-    # Find and replace in JavaScript files in the production environment
     find '/usr/share/nginx/html' -type f -name "*.js" -exec sed -i "s|this.apiUrl=\"[^\"]*\"|this.apiUrl=\"${API_URL}\"|" {} \;
 fi
 
-# Execute the final command (e.g., Nginx start or any other command passed)
 exec "$@"
