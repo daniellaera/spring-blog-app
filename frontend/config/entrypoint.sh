4cb046535a0cb2cdee289aa9178501f149743779
Daniel Laera <laera.daniel@hotmail.com>
2024-11-07 17:40:37 +0100

    ak ya

--- frontend/config/entrypoint.sh
+++ frontend/config/entrypoint.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+echo "Starting entrypoint script..."
+echo "API_URL=$API_URL"
+
 # Ensure the API_URL is replaced in runtime files if needed
 if [ -n "$API_URL" ]; then
     echo "Replacing API_URL in runtime files"
     sed -i "s|\"${API_URL}\"|\"$API_URL\"|g" /usr/share/nginx/html/*.js
+else
+    echo "No API_URL provided"
 fi
 
-# Run nginx in the foreground
+# Run nginx
 exec "$@"
