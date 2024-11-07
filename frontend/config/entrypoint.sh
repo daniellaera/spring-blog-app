7cdc9c01ac4c0aea9990c7270208ebf80ba79fa5
Daniel Laera <laera.daniel@hotmail.com>
2024-11-07 16:39:16 +0100

    ts use files

--- frontend/config/entrypoint.sh
+++ frontend/config/entrypoint.sh
@@ -1,21 +1,27 @@
 #!/bin/sh
 
-echo "Starting environment variable replacement in JavaScript files..."
+# Replacement for the dev environment (targeting environment.development.ts)
+if [ -n "$API_URL" ] && [ -f 'src/environments/environment.development.ts' ]; then
+    echo "Replacing API_URL in environment.development.ts..."
 
-# Check if the API_URL is set and the target directory exists
+    # Replacing the API URL in the development environment configuration
+    sed -i "s|apiUrl: '.*',|apiUrl: '${API_URL}',|" src/environments/environment.development.ts
+fi
+
+# Replacement for the prod environment (targeting environment.ts)
+if [ -n "$API_URL" ] && [ -f 'src/environments/environment.ts' ]; then
+    echo "Replacing API_URL in environment.ts..."
+
+    # Replacing the API URL in the production environment configuration
+    sed -i "s|apiUrl: '.*',|apiUrl: '${API_URL}',|" src/environments/environment.ts
+fi
+
+# Replacement for the prod environment (targeting /usr/share/nginx/html/*.js files)
 if [ -n "$API_URL" ] && [ -d '/usr/share/nginx/html' ]; then
-    echo "Replacing ${API_URL} in JavaScript files with value: $API_URL"
+    echo "Replacing API_URL in JavaScript files in /usr/share/nginx/html..."
 
-    # Replace the placeholder ${API_URL} with the actual value of the environment variable
-    find '/usr/share/nginx/html' -type f -name "*.js" -exec sed -i "s|\${API_URL}|$API_URL|g" {} \;
-
-    # Debug: Confirm the replacement by showing first few lines of a file
-    echo "First few lines of the first .js file after replacement:"
-    # Using $(find ...) to show the first file and its first 10 lines
-    # shellcheck disable=SC2046
-    head -n 10 $(find /usr/share/nginx/html -type f -name "*.js" | head -n 1)
-else
-    echo "API_URL is not set or target directory '/usr/share/nginx/html' does not exist!"
+    # Find and replace in JavaScript files in the production environment
+    find '/usr/share/nginx/html' -type f -name "*.js" -exec sed -i "s|this.apiUrl=\"[^\"]*\"|this.apiUrl=\"${API_URL}\"|" {} \;
 fi
 
 # Execute the final command (e.g., Nginx start or any other command passed)
