4829aa99e3e7989b3551c47941ae10d6b3cbfcc8
Daniel Laera <laera.daniel@hotmail.com>
2024-11-07 15:25:36 +0100

    new shell and dockerifle entrypoint

--- frontend/config/entrypoint.sh
+++ frontend/config/entrypoint.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
-# Check if API_URL is set and replace placeholder in production build files
+echo "Starting environment variable replacement in JavaScript files..."
+
 if [ -n "$API_URL" ]; then
-    echo "Replacing API_URL in production build files..."
-    find '/usr/share/nginx/html' -type f -name "*.js" -exec sed -i "s|\${API_URL}|${API_URL}|g" {} \;
+    echo "Replacing API_URL with value: $API_URL"
+    find '/usr/share/nginx/html' -type f -name "*.js" -exec sed -i "s|\\${API_URL}|$API_URL|g" {} \;
+else
+    echo "API_URL environment variable is not set!"
 fi
 
+# Start Nginx
 exec "$@"
