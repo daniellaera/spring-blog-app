44b99ba3554d1d207deaf43727abc63abc750b7f
Daniel Laera <laera.daniel@hotmail.com>
2025-01-05 17:51:45 +0100

    lets stry thisy

--- backend/src/main/java/com/daniellaera/blog_app/controller/HelloController.java
+++ backend/src/main/java/com/daniellaera/blog_app/controller/HelloController.java
@@ -13,6 +13,6 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Hello World Jeez and!";
+        return "Hello World of COersyx";
     }
 }
