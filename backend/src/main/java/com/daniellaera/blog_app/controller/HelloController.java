a3400ba13043e11051ec2778e3175daa6c97c9ad
Daniel Laera <laera.daniel@hotmail.com>
2025-01-05 17:26:06 +0100

    pr version & controller

--- backend/src/main/java/com/daniellaera/blog_app/controller/HelloController.java
+++ backend/src/main/java/com/daniellaera/blog_app/controller/HelloController.java
@@ -13,6 +13,6 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Hello World and Coders!";
+        return "Hello World Jeez, test that!";
     }
 }
