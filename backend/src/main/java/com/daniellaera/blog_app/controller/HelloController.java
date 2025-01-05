249da00eb76832cef7c0d443f5e081274b3c466e
Daniel Laera <laera.daniel@hotmail.com>
2025-01-05 17:01:54 +0100

    new ocmmt2 controller

--- backend/src/main/java/com/daniellaera/blog_app/controller/HelloController.java
+++ backend/src/main/java/com/daniellaera/blog_app/controller/HelloController.java
@@ -13,6 +13,6 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Hello World!";
+        return "Hello World of Spring Boot Application!";
     }
 }
